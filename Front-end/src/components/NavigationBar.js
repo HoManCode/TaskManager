1bf01f5c5cfa11e175702109acad1782816c6891
Hooman Shykhipour <59225708+HoManCode@users.noreply.github.com>
2024-02-15 14:00:25 +1100

    show login and signup if user is not logged in and show logout if user is logged in

--- Front-end/src/components/NavigationBar.js
+++ Front-end/src/components/NavigationBar.js
@@ -2,8 +2,10 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { useUser } from '../services/UserProvider';
 
 function NavigationBar() {
+  const user = useUser();
   return (
     <>
       <br />
@@ -12,9 +14,11 @@ function NavigationBar() {
           <Navbar.Brand href="/">Task Management System</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/login">Login</Nav.Link>
-            <Nav.Link href="/logout">Logout</Nav.Link>
-            <Nav.Link href="/signup">Sign Up</Nav.Link>
+            { user ? 
+                    <Nav.Link href="/logout">Logout</Nav.Link>
+                   : <Nav.Link href="/login">Login</Nav.Link> }
+            { !user &&
+              <Nav.Link href="/signup">Sign Up</Nav.Link>} 
           </Nav>
         </Container>
       </Navbar>
