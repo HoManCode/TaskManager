33c168c97e0d9fbbe39311b7d70fd2fa5802c8e8
Hooman Shykhipour <59225708+HoManCode@users.noreply.github.com>
2024-03-10 16:58:40 +1100

    create first user repository unit test

--- Back-end/src/test/java/com/TaskManagement/TM/Tests/repository/UserRepositoryTests.java
+++ Back-end/src/test/java/com/TaskManagement/TM/Tests/repository/UserRepositoryTests.java
@@ -1,4 +1,43 @@
 package com.TaskManagement.TM.Tests.repository;
 
+
+import com.TaskManagement.TM.dto.UserDto;
+import com.TaskManagement.TM.model.User;
+import com.TaskManagement.TM.repository.UserRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.HashSet;
+
+import static com.TaskManagement.TM.Enum.Authority.ROLE_ADMIN;
+
+@DataJpaTest
+@ActiveProfiles("test")
 public class UserRepositoryTests {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @BeforeEach
+    public void init(){
+
+    }
+
+    @Test
+    public void UserRepository_SaveAll_ReturnSavedUser(){
+
+        //Arrange
+        User user = new User("dd", "hoo",new HashSet<>(ROLE_ADMIN.ordinal()));
+
+        //Act
+        User savedUser = userRepository.save(user);
+
+        //Assert
+        Assertions.assertThat(savedUser).isNotNull();
+        Assertions.assertThat(savedUser.getId()).isGreaterThan(0);
+    }
 }
