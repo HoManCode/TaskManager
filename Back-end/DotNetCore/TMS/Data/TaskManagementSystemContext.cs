891ccb5066d844cde5a320a51175d981fa299616
Hooman Shykhipour <59225708+HoManCode@users.noreply.github.com>
2024-03-17 22:07:56 +1100

    update gitignore file

--- Back-end/DotNetCore/TMS/Data/TaskManagementSystemContext.cs
+++ Back-end/DotNetCore/TMS/Data/TaskManagementSystemContext.cs
@@ -25,11 +25,7 @@ public partial class TaskManagementSystemContext : DbContext
     public virtual DbSet<Task> Tasks { get; set; }
 
     public virtual DbSet<User> Users { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseMySQL("Data Source=localhost;Database=task_management_system;Port=3306;Uid=root;Pwd=T1757155082t?;");
-
+    
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Authority>(entity =>
