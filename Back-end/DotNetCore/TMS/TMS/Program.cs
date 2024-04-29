04741dad8721e920b771dc3ca59b22bbe764bbc7
Hooman Shykhipour <59225708+HoManCode@users.noreply.github.com>
2024-04-29 16:12:50 +1000

    add appsettings.json to gitignore file and setup connection string

--- Back-end/DotNetCore/TMS/TMS/Program.cs
+++ Back-end/DotNetCore/TMS/TMS/Program.cs
@@ -1,9 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using TMS.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+var serverVersion = new MySqlServerVersion(new Version(8, 0, 36));
+var connectionString = builder.Configuration.GetConnectionString("MySqlConnection");
+builder.Services.AddDbContext<TMSContext>(options => options.UseMySql(connectionString, serverVersion));
 
 var app = builder.Build();
 
